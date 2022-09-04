1162b36fe8492448e993e7746f705c9a407758fc
Johan Sebastian Reyes Prieto <jsreyes463@misena.edu.co>
2022-09-04 11:09:28 -0500

    Módulo palabra y temperatura hechos

--- js/tempertatura.js
+++ js/tempertatura.js
@@ -1,21 +1,76 @@
-const gradosUsu=document.getElementById("grados")
 
-const conversion1=document.getElementById("conver1")
+function calcular() {
 
-const conversion2=document.getElementById("conver2")
-
-const calc=document.getElementById("calcular")
-let conver=0
+    let conversion1=document.getElementById("conver1").value
+    let conversion2=document.getElementById("conver2").value
+    let gradosUsu=parseFloat(document.getElementById("grados").value)
 
 
-let conversion=()=> {
-    switch (gradosUsu) {
-        case value:
-            
-            break;
+ switch (conversion1) {
+
+
+
+    case 'Celsius':
+        if (conversion2=='Celsius') {
+            alert("No podemos realizar la conversión de Celsius a Celsius");
+        }
+        else if(conversion2=='Fahrenheit'){
+            console.log("Vamos a realizar la conversión de Celsius a Fahrenheit");
+            let resultado=(gradosUsu*1.8)+32
+
+            alert("Sus grados en Faherenheit son: "+resultado)
+        }
+
+        else{
+            console.log("Vamos a realizar la conversión de Celsius a Kelvin");
+            let resultado=gradosUsu+273.15;
+            alert("Sus grados Kelvin son: "+resultado)
+        }
+        break;
+
+
+
     
-        default:
-            break;
-    }
+    case 'Fahrenheit':
+          if (conversion2=='Fahrenheit') {
+            alert("No podemos realizar la conversión de Fahrenheit a Fahrenheit");
+        }
+        else if(conversion2=='Kelvin'){
+            console.log("Vamos a realizar la conversión de Fahrenheit a Kelvin");
+            let resultado=  0.55*(gradosUsu-32)+273;
+            alert("Sus grados Kelvin son: "+resultado)
+        }
+
+        else{
+            console.log("Vamos a realizar la conversión de Fahrenheit a Celsius");
+            let resultado=(gradosUsu -32) / 1.8; 
+            alert("Sus grados Celsius son: "+resultado)
+        }
+        break;
+    
+    case 'Kelvin':
+        if (conversion2=='Kelvin') {
+            alert("No podemos realizar la conversión de Kelvin a Kelvin");
+        }
+        else if(conversion2=='Fahrenheit'){
+            console.log("Vamos a realizar la conversión de Kelvin a Fahrenheit");
+            let resultado=1.8* (gradosUsu-273.15) + 32;
+            alert("Vamos a realizar la conversión de Kelvin a Fahrenheit: "+resultado);
+        }
+
+        else{
+            console.log("Vamos a realizar la conversión de Kelvin a Celsius");
+            let resultado=gradosUsu-273.15;
+            alert("Vamos a realizar la conversión de Kelvin a Celsius: "+resultado);
+        }
+        break;
+    
+    default:
+        break;
+ }
+  
 }
 
+const conversion=document.getElementById('calcular')
+
+conversion.addEventListener('click',calcular)
